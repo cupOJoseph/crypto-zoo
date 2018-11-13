484b14f444d9619d7312ed36c495ea86caff275a
randallkanna <randallkanna@gmail.comm>
2018-11-12 20:52:53 -0800

    removes errors

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,6 +6,14 @@ import Zoo from '../Zoo';
 import Airbus from '../Airbus';
 import Give from '../Give';
 
+import {
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+
 const HomeRoute = () => (
   <Home/>
 );
@@ -30,8 +38,30 @@ class App extends Component {
   render() {
     return (
       <div>
+
+      <Navbar className="navbar" color="light" light expand="md">
+          <NavbarBrand href="/">Nifty Zoo</NavbarBrand>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/national-zoo">National Zoo</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/airbus">Airbus Heritage</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/give">Give</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/profile">My Zoo</NavLink>
+              </NavItem>
+            </Nav>
+        </Navbar>
+
         <Route path="/" exact component={HomeRoute}/>
         <Route path="/profile" exact component={ProfileRoute}/>
+        <Route path="/national-zoo" exact component={ZooRoute}/>
+        <Route path="/airbus" exact component={AirbusRoute}/>
+        <Route path="/give" exact component={GiveRoute}/>
       </div>
     );
   }
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,56 +1,11 @@
 import React, { Component } from 'react';
 
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem } from 'reactstrap';
-
 import './style.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
-  }
-
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    })
-  }
-
   render() {
     return (
       <div className="App">
-      <Navbar className="navbar" color="light" light expand="md">
-          <NavbarBrand href="/">Nifty Zoo</NavbarBrand>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="/national-zoo">National Zoo</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/airbus">Airbus Heritage</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/give">Give</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/profile">My Zoo</NavLink>
-              </NavItem>
-            </Nav>
-        </Navbar>
 
         <div className="image"></div>
 
