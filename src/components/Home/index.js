01d3e297a6a2905359a7a29e283ea29bca1a436c
randallkanna <randallkanna@gmail.comm>
2018-11-29 12:43:54 -0800

    fixed image

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,9 +7,15 @@ class App extends Component {
     return (
       <div className="App">
 
-        <div className="image"></div>
+        <div className="image">
+          <div>
+            <h1>Collect, Trade and Build your Collection of Pets</h1>
+            <h2 className="hero-title">100% of funds support Muttville.</h2>
+          </div>
+        </div>
 
-        <div className="placeholder-info">
+
+        <div className="about">
         </div>
 
       </div>
