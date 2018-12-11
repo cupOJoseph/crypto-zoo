b25e266a1739d821b9f121f8db12293d9facf30c
randallkanna <randallkanna@gmail.comm>
2018-12-11 11:40:01 -0800

    in prog

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -2,24 +2,78 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle, Row, Col } from 'reactstrap';
 import './style.css';
+import web3 from '../web3';
+const noWeb3ErrorMessage =
+  'Could not connect to your web3 wallet. Please ensure your wallet is unlocked, and you are using the "Main Ethereum Network". ';
+const noWeb3WalletFound = (
+  <div>
+    It looks like you do not have an ethereum wallet, or it is not enabled. In
+    order to claim your token, please install{' '}
+    <a href="http://metamask.io" target="_blank">
+      Metamask
+    </a>{' '}
+    and refresh the page.
+  </div>
+);
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      animals: [{
-        name: 'Rover',
-        image: 'http://petmd.com/sites/default/files/pekingese-dog.jpg',
-        subtitle: 'Valuable',
-        description: 'A little more info'
-      }, {
-        name: 'Sam',
-        image: 'https://www.gannett-cdn.com/-mm-/d8427785073ef3bf0463f168465a2564fdd37ce8/c=160-0-1440-1280/local/-/media/USATODAY/USATODAY/2013/11/24//1385325676002-GAN-TALKING-DOG-112413-3.jpg?width=200&height=200&fit=crop',
-        subtitle: 'Rare dog breed',
-        description: 'A little more info'
-      }]
+      animals: [],
+      accounts: [],
+      metamaskEnabled: false,
+      errorMessage: false,
     };
+  }
+
+  componentWillMount() {
+    this.enableWeb3();
+  }
+
+  componentDidMount() {
+    this.getTokenList();
+  }
+
+  enableWeb3 = () => {
+    if (window.ethereum) {
+      try {
+        window.ethereum.enable().then(() => {
+          window.web3.eth.getAccounts((err, accounts) => {
+            console.log('metamask enabled!');
+            debugger;
+            this.setState({ metamaskEnabled: true, accounts });
+          });
+        });
+      } catch (e) {
+        // console.log(e);
+      }
+    }
+  };
+
+  getTokenList() {
+    let {
+      accounts,
+      metamaskEnabled
+    } = this.state;
+
+    debugger;
+
+     try {
+       if (!metamaskEnabled) {
+         debugger;
+         this.setState({ errorMessage: noWeb3WalletFound });
+       } else {
+         debugger;
+         let account = accounts[0];
+         debugger;
+         fetch(`https://heritage-api.glitch.me/api/user?address=${accounts}`)
+         .then(({ results }) => this.setState({ tokens: results }));
+       }
+     } catch (err) {
+       this.setState({ errorMessage: noWeb3ErrorMessage });
+     }
   }
 
   render() {
